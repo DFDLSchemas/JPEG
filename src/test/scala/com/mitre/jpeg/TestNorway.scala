94a551233863b5b1f36f2911346963182b34a2a2
Josh Adams <jadams@owlcyberdefense.com>
2025-04-23 10:28:22 -0400

    Small changes for Scala 3 compatability

--- src/test/scala/com/mitre/jpeg/TestNorway.scala
+++ src/test/scala/com/mitre/jpeg/TestNorway.scala
@@ -11,17 +11,17 @@ object TestNorway {
     compileAllTopLevel = true)
 
   @AfterClass def shutdown() = {
-    runner.reset
+    runner.reset()
   }
 }
 
 class TestNorway {
   import TestNorway._
 
-  @Test def test_Norway() { runner.runOneTest("Norway") }
-  @Test def test_notEmptyJPEG() { runner.runOneTest("notEmptyJPEG") }
-  @Test def test_missingEOI1() { runner.runOneTest("missingEOI1") }
-  @Test def test_notDataBlob1() { runner.runOneTest("notDataBlob1") }
-  @Test def test_minimal1() { runner.runOneTest("minimal1") }
-  @Test def test_penguins() { runner.runOneTest("Penguins") }
+  @Test def test_Norway() = { runner.runOneTest("Norway") }
+  @Test def test_notEmptyJPEG() = { runner.runOneTest("notEmptyJPEG") }
+  @Test def test_missingEOI1() = { runner.runOneTest("missingEOI1") }
+  @Test def test_notDataBlob1() = { runner.runOneTest("notDataBlob1") }
+  @Test def test_minimal1() = { runner.runOneTest("minimal1") }
+  @Test def test_penguins() = { runner.runOneTest("Penguins") }
 }
